114a26b3261f3484641dfb75a53accfa58fd942a
Flavio Lovisa <l-o-v-i-s-a91@hotmail.it>
2023-08-02 18:20:56 +0200

    Add function to gather book information through input values

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    title = this[0].value;
+    author = this[1].value;
+    pages = this[2].value;
+    read = this[3].checked;
+    initialBookIndex = bookLibrary.length;
+
+    this[0].value = '';
+    this[1].value = '';
+    this[2].value = '';
+    this[3].checked = false;
+});
+
 const showModalBox = function () {
     const showModalButton = document.querySelector('.show-modal-button');
     const mask = document.querySelector('.mask');
